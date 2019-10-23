2ebb8758238c1ec7913d5b1f4b31f6580e220967
chaidaxuan <1032127641@qq.com>
2019-10-23 18:22:30 +0800

    first commit

---
+++ cx/src/components/login.ts
@@ -0,0 +1,9 @@
+import wepy from "wepy";
+export default class Login extends wepy.page {
+    Phone: string;
+    Password: string;
+    public data = {
+        Phone: 18521519605,
+        Password: 123456
+    };
+}
